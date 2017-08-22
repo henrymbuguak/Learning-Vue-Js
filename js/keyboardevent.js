0300466262dcada9241e98f528be46622820493c
henrymbuguak <henrymbuguak@gmail.com>
2017-08-22 12:20:16 +0300

    Keyboard event

---
+++ js/keyboardevent.js
@@ -0,0 +1,33 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    title: "Learning Vue Js Event",
+    age: 25,
+    x: 0,
+    y: 0
+  },
+  methods: {
+    greet: function(time) {
+      return 'Welcome ' + time + this.name + ' Tutorial';
+    },
+    add: function (inc) {
+      this.age += inc;
+    },
+    subtract: function (dec) {
+      this.age -= dec;
+    },
+    updateXY: function(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+    },
+    click: function() {
+      alert('You clicked me!');
+    },
+    logName: function() {
+      console.log('you entered your name.');
+    },
+    logAge: function() {
+      console.log('you entered your age.');
+    }
+  }
+});
